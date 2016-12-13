8c91af94cb72e1927d9db2df08c9e8265cf6b722
darkcode357 <darkcode357@gmail.com>
2016-12-13 18:03:24 -0200

    Update blackarchV1.sh

--- blackarchV1.sh
+++ blackarchV1.sh
@@ -5,7 +5,22 @@
 # repositorio do criador https://github.com/darkcode357       #
 ###############################################################
 #-infos-------------------------------------------------------#
-# configuraçao do sistema para um ambiente melhor de pentest  #
+# script voltado para configuração das  ferramentas dos       #
+# do blackarch linux ou no proprio arch linux com o repositoio#
+# voltado para configuração de de 1606  ferramentas e         #
+# configuração manual do metasploit , fazendo criar a sua     # 
+# propria database do metasploit e                            #
+# configuração e instalação                                   #
+#cobaltstrike 2.7 e script cortana para aux do metasploit     #     
 ###############################################################
 #-system------------------------------------------------------#
-# versao 1.0 
+# versao 1.0 blackarchV1.sh                                   #
+###############################################################
+#-licença-----------------------------------------------------#
+#  MIT licença {+} http://opensource.org/licenses/MIT         #
+###############################################################
+#-lembretes-para-o-usuario------------------------------------#
+# roda esse script como usuario root para voce pode fazer     #
+# todas as configuraçoes 
+
+
